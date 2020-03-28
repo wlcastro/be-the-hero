22002beb58566bece2cef2bbea7dedb6cdb0a263
wlcastro <wander.castro@gmail.com>
2020-03-27 23:02:34 -0300

    mobile-fim

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,10 +1,83 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import * as MailComponser from 'expo-mail-composer';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
 
 export default function Detail() {
+  const navigation = useNavigation();
+  const route =useRoute();
+
+  const incident = route.params.incident;
+  const message = `Oi ${incident.name}, estou entrando em contado pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR',{
+    style:'currency',
+    currency:'BRL'
+    }).format(incident.value)}`
+
+  function navigateBack(){
+    navigation.goBack()
+  }
+
+  function sendMail(){
+    MailComponser.composeAsync({
+      subject:`Heroi do caso: ${incident.title}`,
+      recipients:[incident.email],
+      body: message
+    })
+  }
+
+  function sendWhatsapp(){
+    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text${message}`)
+  }
+
   return (
-    <View />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity
+            style={styles.detailsButton}
+            onPress={navigateBack}
+          >
+            <Feather name="arrow-left"  
+              size={16} 
+              color="#e02041"/>
+          </TouchableOpacity>
+      </View>
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty,{marginTop:0}]}>ONG:</Text>
+        <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.fu}</Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>{incident.title}</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>
+          {Intl.NumberFormat('pt-BR',{
+            style:'currency',
+            currency:'BRL'
+            }).format(incident.value)}
+        </Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+        <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
+            <Text style={styles.actionText}>eMail</Text> 
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
--- mobile/src/pages/Incident/styles.js
+++ mobile/src/pages/Incident/styles.js
@@ -7,18 +7,22 @@ export default StyleSheet.create({
     paddingHorizontal:24,
     paddingTop: Constants.statusBarHeight +20
   },
+
   header:{
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center'
   },
+
   headerText:{
     fontSize:15,
     color: '#737380'
   },
+
   headerTextBold: {
     fontWeight: 'bold'
   },
+
   title:{
     fontSize:30,
     marginBottom: 1,
@@ -26,10 +30,47 @@ export default StyleSheet.create({
     color: '#13131a',
     fontWeight: 'bold'
   },
+
   description:{
     fontSize:16,
     lineHeight: 24,
     color:'#737380'
+  },
+
+  incidentList:{
+    marginTop:32
+  },
+
+  incident:{
+    padding:24,
+    borderRadius:8,
+    backgroundColor:'#fff',
+    marginBottom: 16,
+  },
+
+  incidentProperty:{
+    fontSize:14,
+    color:'#41414d',
+    fontWeight:'bold',
+  },
+
+  incidentValue:{
+    marginTop:8,
+    fontSize:15,
+    marginBottom:24,
+    color:'#737380'
+  },
+
+  detailsButton: {
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  detailsButtonText: {
+    color:'#e02041',
+    fontSize:15,
+    fontWeight:'bold'
   }
 
 });
